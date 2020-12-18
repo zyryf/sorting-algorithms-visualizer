ee419f8e435f3df9cb00d3dddc5a52e8c45bf0a7
Szymon Borkowski <szymon.borkowski@gmail.com>
2020-12-18 20:22:04 +0100

    nothing works

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -9,12 +9,19 @@ const BarContent = styled.div`
   margin: 0 0.2rem;
   transition: 0.5s;
 `;
+const BarWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  height: 100%;
+`;
 
 const Bar = ({ height, color }) => {
   return (
-    <>
+    <BarWrapper>
       <BarContent height={height} color={color} />
-    </>
+      <p>{height}</p>
+    </BarWrapper>
   );
 };
 
--- src/components/sorting_components/InsertionSort.js
+++ src/components/sorting_components/InsertionSort.js
@@ -1,60 +1,37 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../../App';
+import { COLORS } from '../../ThemeConstants';
 
 import { SortButton } from './SortButton';
 
 const InsertionSort = () => {
   let { data, setData } = useContext(AppContext);
 
+  const restoreColors = () => {
+    data.forEach((el) => {
+      el.color = COLORS.light_blue;
+    });
+    setData([...data]);
+  };
+
+  const setColor = (color, index) => {
+    data[index].color = color;
+    setData([...data]);
+  };
+
   const insertionSort = () => {
-    let n = data.length;
     let delay = 0;
-    for (let i = 1; i < n; i++) {
-      delay += 100;
-      let second_delay = 0;
-      let iterationsNumber = 0;
-      // Choosing the first element in our unsorted subarray
+    let second_delay = 0;
+    for (let i = 1; i < data.length; i++) {
+      let j = i - 1;
       let current = data[i];
 
-      // The last element of our sorted subarray
-      let j = i - 1;
-
-      // copyting the data
-      let copyOfData = [...data];
-      let copyOfCurrent = { ...current };
-      let copyOfJ = j;
-
-      // helper loop to check iterations
-      while (j > -1 && current.value < data[j].value) {
+      for (j; j > -1 && current.value < data[j].value; j--) {
         data[j + 1] = data[j];
-        iterationsNumber++;
-        j--;
-      }
-      // assigning back the original data
-      j = copyOfJ;
-      data = copyOfData;
-      current = copyOfCurrent;
-
-      setTimeout(() => {
-        console.log(`pierwszy`);
-        while (j > -1 && current.value < data[j].value) {
-          second_delay += 100;
-          setTimeout(() => {
-            console.log(copyOfJ);
-            data[copyOfJ + 1] = data[copyOfJ];
-            setData([...data]);
-            copyOfJ--;
-          }, second_delay);
-
-          j--;
-        }
-      }, delay);
-      delay += (iterationsNumber + 1) * 100;
-      setTimeout(() => {
-        console.log('drugi');
-        data[j + 1] = current;
         setData([...data]);
-      }, delay);
+      }
+      data[j + 1] = current;
+      setData([...data]);
     }
   };
 
