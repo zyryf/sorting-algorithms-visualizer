765f3913173f9a868bc5b627ac7e0eabdb781dd9
Szymon Borkowski <szymon.borkowski@gmail.com>
2020-12-18 01:07:36 +0100

    first prototype of insetion sort with delay

--- src/components/sorting_components/InsertionSort.js
+++ src/components/sorting_components/InsertionSort.js
@@ -5,37 +5,37 @@ import { SortButton } from './SortButton';
 
 const InsertionSort = () => {
   const { data, setData } = useContext(AppContext);
+  let delay = 500;
 
   const insert = (data, rightIndex, element) => {
-    for (
-      rightIndex;
-      rightIndex >= 0 && data[rightIndex].value > element.value;
-      rightIndex--
-    ) {
-      data[rightIndex + 1] = data[rightIndex];
-    }
-    data[rightIndex + 1] = element;
+    delay += 500;
+    setTimeout(() => {
+      for (
+        rightIndex;
+        rightIndex >= 0 && data[rightIndex].value > element.value;
+        rightIndex--
+      ) {
+        data[rightIndex + 1] = data[rightIndex];
+        setData([...data]);
+      }
+
+      data[rightIndex + 1] = element;
+      setData([...data]);
+    }, delay);
   };
 
   const insertionSort = () => {
     for (let i = 1; i < data.length; i++) {
       insert(data, i - 1, data[i]);
     }
-    setData([...data]);
-  };
-
-  const performInsetionSort = () => {
-    insertionSort();
-    /*
-    why does setData(sortedData) not work ???
-    */
   };
 
   return (
     <div>
       <SortButton
         onClick={() => {
-          performInsetionSort();
+          // test();
+          insertionSort();
         }}
       >
         Insertion Sort
